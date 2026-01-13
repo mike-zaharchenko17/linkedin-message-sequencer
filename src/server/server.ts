c75dbca9ed7858063a049be3476ede89713b9f50
mike-zaharchenko17 <mz2433@nyu.edu>
2026-01-13 00:23:11 -0500

    properly configured port for render

--- src/server/server.ts
+++ src/server/server.ts
@@ -207,11 +207,19 @@ const routes = async (fastify : FastifyInstanceWithProvider) => {
 
 fastify.register(routes)
 
+fastify.register(routes);
+
 export const runServer = async () => {
     try {
-        await fastify.listen({ port: 3000 })
+        const port = Number(process.env.PORT) || 3000;
+
+        await fastify.listen({
+            port,
+            host: "0.0.0.0",
+        });
+
     } catch (err) {
-        fastify.log.error(err)
-        process.exit(1)
+        fastify.log.error(err);
+        process.exit(1);
     }
-}
+};
